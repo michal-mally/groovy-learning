6db70df40befc598c98fe3da7a001784599b6aee
Michal Mally <michal@mally.pl>
2013-02-14 04:07:08 +0100

    Time.groovy script simplified.

--- episode-4-gpars/src/main/groovy/pl/helenium/learning/groovy/gpars/Time.groovy
+++ episode-4-gpars/src/main/groovy/pl/helenium/learning/groovy/gpars/Time.groovy
@@ -2,21 +2,12 @@ package pl.helenium.learning.groovy.gpars
 
 import org.apache.commons.lang3.time.StopWatch
 
-final class Time {
-
-    static void time(Map params, Closure<?> closure) {
+static void time(Map params, Closure<?> closure) {
+    (params.times ?: 5).times {
         def watch = new StopWatch()
-        (params.times?:5).times {
-            watch.reset()
-            watch.start()
-            closure()
-            watch.stop()
-            println "Task ${params.id} took ${watch.time}ms to complete."
-        }
+        watch.start()
+        closure()
+        watch.stop()
+        println "Task ${params.id} took ${watch.time}ms to complete."
     }
-
-    private Time() {
-        // intentionally left blank
-    }
-
 }
