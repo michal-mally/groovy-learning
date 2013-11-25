6b7cc724ae1e43e1412cfe5733e9cd461df8a8e2
Michal Mally <michal@mally.pl>
2013-11-25 21:16:04 +0100

    Groovy Training Part 2: OrderTest rewritten using Spock. TestNG replaced by JUnit.

--- training-part2-testing/01-groovy-unit-test-introduction/src/test/groovy/pl/softwaremind/ckjava/recommendation/groovy/training/OrderTest.groovy
+++ training-part2-testing/01-groovy-unit-test-introduction/src/test/groovy/pl/softwaremind/ckjava/recommendation/groovy/training/OrderTest.groovy
@@ -1,102 +1,92 @@
 package pl.softwaremind.ckjava.recommendation.groovy.training
 
-import org.testng.annotations.Test
+import spock.lang.Specification
 
-class OrderTest {
+class OrderTest extends Specification {
 
-    @Test(expectedExceptions = OrderException)
-    void 'shall not allow to close empty order'() {
-        // given
+    def 'shall not allow to close empty order'() {
+        given:
         def order = new Order('order number 1234')
 
-        // when
+        when:
         order.close()
 
-        // then
-        // exception expected
+        then:
+        thrown(OrderException)
     }
 
-    @Test(expectedExceptions = OrderException)
-    void 'shall not allow to add items to already closed order'() {
-        // given
+    def 'shall not allow to add items to already closed order'() {
+        given:
         def order = new Order('order number 1234')
                 .addItem(OrderItem.withDefault())
                 .close()
 
-        // when
+        when:
         order.addItem(OrderItem.withDefault())
 
-        // then
-        // exception expected
+        then:
+        thrown(OrderException)
     }
 
-    @Test
-    void 'shall cLose on already closed order have no effect'() {
-        // given
+    def 'shall close on already closed order have no effect'() {
+        given:
         def order = new Order('order number 1234')
                 .addItem(OrderItem.withDefault())
                 .close()
 
-        // when
+        when:
         order.close()
 
-        // then
-        assert order.closed
+        then:
+        order.closed
     }
 
-    @Test(expectedExceptions = OrderException)
-    void 'shall not allow to add item with the same code twice'() {
-        // given
+    def 'shall not allow to add item with the same code twice'() {
+        given:
         def item = OrderItem.withDefault()
         def order = new Order('order number 1234')
                 .addItem(item)
 
-        // when
+        when:
         order.addItem(OrderItem.with(code: item.code))
 
-        // then
-        // exception expected
+        then:
+        thrown(OrderException)
     }
 
-    @Test
-    void 'shall find added item by it\'s code'() {
-        // given
+    def 'shall find added item by it\'s code'() {
+        given:
         def item = OrderItem.withDefault()
         def order = new Order('order number 1234')
                 .addItem(item)
 
-        // when
-        // then
-        assert order.getItemByCode(item.code)?.is(item)
+        expect:
+        order.getItemByCode(item.code)?.is(item)
     }
 
-    @Test
-    void 'shall return null if item is not found by code'() {
-        // given
+    def 'shall return null if item is not found by code'() {
+        given:
         def item = OrderItem.withDefault()
         def order = new Order('order number 1234')
                 .addItem(item)
 
-        // when
-        // then
-        assert order.getItemByCode("other item code than $item.code") == null
+        expect:
+        order.getItemByCode("other item code than $item.code") == null
     }
 
-    @Test(expectedExceptions = UnsupportedOperationException)
-    void 'shall not allow to add items to collection obtained by getItems()'() {
-        // given
+    def 'shall not allow to add items to collection obtained by getItems()'() {
+        given:
         def order = new Order('order number 1234')
 
-        // when
+        when:
         order.items.add(OrderItem.withDefault())
 
-        // then
-        // exception expected
+        then:
+        thrown(UnsupportedOperationException)
     }
 
-    @Test
-    void 'shall return all the items added when getItems() called in the same order as added'() {
-        // given
+    def 'shall return all the items added when getItems() called in the same order as added'() {
+        given:
         def order = new Order('order number 1234')
 
         def itemsAdded = []
@@ -105,14 +95,13 @@ class OrderTest {
             order.addItem(item)
             itemsAdded << item
         }
-        // when
-        // then
-        assert order.items as List == itemsAdded
+
+        expect:
+        order.items as List == itemsAdded
     }
 
-    @Test
-    void 'shall return correct net total'() {
-        // given
+    def 'shall return correct net total'() {
+        given:
         def order = new Order('order number 1234')
 
         List<OrderItem> itemsAdded = []
@@ -122,14 +111,12 @@ class OrderTest {
             itemsAdded << item
         }
 
-        // when
-        // then
-        assert order.netTotal == itemsAdded*.netTotal.sum()
+        expect:
+        order.netTotal == itemsAdded*.netTotal.sum()
     }
 
-    @Test
-    void 'shall return correct gross total'() {
-        // given
+    def 'shall return correct gross total'() {
+        given:
         def order = new Order('order number 1234')
 
         List<OrderItem> itemsAdded = []
@@ -139,9 +126,8 @@ class OrderTest {
             itemsAdded << item
         }
 
-        // when
-        // then
-        assert order.grossTotal == itemsAdded*.grossTotal.sum()
+        expect:
+        order.grossTotal == itemsAdded*.grossTotal.sum()
     }
 
 }
