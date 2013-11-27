91138e669043b4c086f7c8314509d9ae94abfe77
Michal Mally <michal@mally.pl>
2013-11-27 01:22:53 +0100

    Groovy Training Part 2: Native Spock mocking.

--- training-part2-testing/01-groovy-unit-test-introduction/src/test/groovy/pl/softwaremind/ckjava/recommendation/groovy/training/service/EmailOrderSenderTest.groovy
+++ training-part2-testing/01-groovy-unit-test-introduction/src/test/groovy/pl/softwaremind/ckjava/recommendation/groovy/training/service/EmailOrderSenderTest.groovy
@@ -1,6 +1,5 @@
 package pl.softwaremind.ckjava.recommendation.groovy.training.service
 
-import org.mockito.ArgumentCaptor
 import pl.softwaremind.ckjava.recommendation.groovy.training.domain.Order
 import pl.softwaremind.ckjava.recommendation.groovy.training.domain.OrderException
 import pl.softwaremind.ckjava.recommendation.groovy.training.domain.OrderItem
@@ -8,14 +7,10 @@ import pl.softwaremind.ckjava.recommendation.groovy.training.mail.Email
 import pl.softwaremind.ckjava.recommendation.groovy.training.mail.EmailSendingException
 import pl.softwaremind.ckjava.recommendation.groovy.training.mail.EmailServer
 import spock.lang.Specification
-import spock.lang.Unroll
-
-import static org.mockito.Matchers.any
-import static org.mockito.Mockito.*
 
 class EmailOrderSenderTest extends Specification {
 
-    def emailServer = mock(EmailServer)
+    def emailServer = Mock(EmailServer)
 
     def from = "no-reply@example.com"
 
@@ -32,10 +27,12 @@ class EmailOrderSenderTest extends Specification {
         then: 'sending is not allowed'
         thrown OrderException
         and: 'there is no interaction with email server'
-        verifyZeroInteractions(emailServer)
+        0 * emailServer._
     }
 
     def 'shall send closed order properly'() {
+        Email email = null
+
         given: 'any closed order'
         def order = new Order('order number 1234')
                 .addItem(OrderItem.withDefault())
@@ -47,9 +44,7 @@ class EmailOrderSenderTest extends Specification {
         orderSender.sendOrder(recipient, order)
 
         then: 'one email is sent'
-        def emailCaptor = new ArgumentCaptor<Email>()
-        verify(emailServer, times(1)).sendEmail(emailCaptor.capture())
-        def email = emailCaptor.value
+        1 * emailServer.sendEmail(_) >> { email = it[0] }
         and: 'has order as attachment'
         email.attachments == [order]
         and: 'has correctly set properties'
@@ -64,28 +59,25 @@ Best regards,
 $from"""
     }
 
-    @Unroll
-    def 'shall succeed in delivering order even if #attemptFails tries fail'() {
+    def 'shall succeed in delivering order even if first 2 tries fail'() {
+        Email email = null
+
         given: 'any closed order'
         def order = new Order('order number 1234')
                 .addItem(OrderItem.withDefault())
                 .close()
         and: 'first (and second) attempt fails'
-        def stubber = doThrow(new EmailSendingException("Error sending email"))
-        if (attemptFails == 2) {
-            stubber = stubber.doThrow(new EmailSendingException("Error sending email"))
-        }
-        stubber.doNothing()
-                .when(emailServer).sendEmail(any(Email))
 
         when: 'order is being sent to recipient'
         orderSender.sendOrder('recipient', order)
 
-        then: 'email is sent'
-        verify(emailServer, times(attemptFails + 1)).sendEmail(any(Email))
-
-        where:
-        attemptFails << [1, 2]
+        then: 'maximum 3 attempts are made'
+        (1..3) * emailServer.sendEmail(_) >>
+                { throw new EmailSendingException("Error sending email") } >>
+                { throw new EmailSendingException("Error sending email") } >>
+                { email = it[0] }
+        and: 'email is sent'
+        email
     }
 
     def 'shall not deliver order if 3 attempts to deliver fails'() {
@@ -94,8 +86,6 @@ $from"""
                 .addItem(OrderItem.withDefault())
                 .close()
         and: 'all attempts to send email fail'
-        doThrow(new EmailSendingException("Error sending email!"))
-                .when(emailServer).sendEmail(any(Email))
 
         when: 'order is being sent to recipient'
         orderSender.sendOrder('recipient', order)
@@ -103,7 +93,7 @@ $from"""
         then: 'sender gives up'
         thrown OrderException
         and: 'maximum 3 attempts are made'
-        verify(emailServer, times(3)).sendEmail(any(Email))
+        3 * emailServer.sendEmail(_) >> { throw new EmailSendingException("Error sending email!") }
     }
 
 }
